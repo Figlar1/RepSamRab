3c848697295492fe585bba263a5133d79080919a
Figlar1 <168334526+Figlar1@users.noreply.github.com>
2024-04-28 17:54:33 +0900

    Add files via upload

---
+++ ConsoleApplication59/ConsoleApplication59/ConsoleApplication59.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+        int sum_of_elems = 0;
+        int num;
+        vector<int> vec;
+        cout << "Please enter sequence of an integers:\n";
+        while (cin >> num)
+            vec.push_back(num);
+        for (vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+            sum_of_elems += *it;
+        cout << sum_of_elems;
+
+        return 0;
+}
+
